0239cd0a6e40693347fc537de0befbc6177795a9
c.borao.2017 <c.borao.2017@alumnos.urjc.es>
2021-05-03 20:06:59 +0200

    Primera versión funcional

---
+++ project/cms_post/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('loggedIn', views.logged_in),
+    path('login', views.login_view),
+    path('logout', views.logout_view),
+    path('annotated/<str:key>', views.get_annotated),
+    path('edit/<str:key>', views.edit),
+    path('<str:key>', views.get_content),
+]
+
